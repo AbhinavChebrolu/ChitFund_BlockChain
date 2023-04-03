28e9174b97c28111ecfb402f12c728f311169376
Abhinav Chebrolu <78219591+AbhinavChebrolu@users.noreply.github.com>
2023-04-03 20:44:30 +0530

    Delete ChitFund/chit-fund-master directory

--- ChitFund/chit-fund-master/ethereum/factory.js
+++
@@ -1,9 +0,0 @@
-import web3 from "./web3";
-import ChitFundFactory from "./build/ChitFundFactory.json";
-
-const instance = new web3.eth.Contract(
-  JSON.parse(ChitFundFactory.interface),
-  "0x2A9a3cC85D866E9B96E68fc24f9ddDDF13d48e10"
-);
-
-export default instance;
--- ChitFund/chit-fund-master/routes.js
+++
@@ -1,8 +0,0 @@
-const routes = require("next-routes")();
-
-routes.add("/fund/new", "/fund/new");
-routes.add("/fund/:address", "/fund/show");
-routes.add("/fund/:address/participants", "/fund/participants");
-routes.add("/fund/:address/requests/new", "/fund/requests/new");
-
-module.exports = routes;
